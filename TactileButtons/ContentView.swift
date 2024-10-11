8c051176144689abda7dbfb3077a1e239636ada9
Wayne Dahlberg <waynefj40@gmail.com>
2024-10-11 13:37:56 -0600

    Initial Commit

---
+++ TactileButtons/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TactileButtons
+//
+//  Created by Wayne Dahlberg on 10/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TactileButtons/TactileButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TactileButtonsApp.swift
+//  TactileButtons
+//
+//  Created by Wayne Dahlberg on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TactileButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
